6b72a904d6c735a07235e8970d6f7741bc8b0b9e
thibmaro <thibault.maroselli@hotmail.fr>
2019-11-22 09:34:38 +0100

    ajout des liens sur les users, débute la page 32

--- src/Controller/AlumniController.php
+++ src/Controller/AlumniController.php
@@ -6,15 +6,27 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AlumniController
+class AlumniController extends AbstractController
 {
     /**
      * @Route("/alumni/{id}/{slug}", name="alumni.index")
      */
-    public function index($id, $slug, UserRepository $userRepository)
+
+    public function index(User $user, $slug)
     {
-        dd($userRepository ->find($id));
+//        dd($user);
+        return $this -> render('alumni/index.html.twig', [
+            'user' => $user
+        ]);
     }
+
+//    Ancienne version, à titre d'exemple pour illustrer
+//    public function index($id, $slug, UserRepository $userRepository)
+//    {
+//        dd($userRepository ->find($id));
+//    }
+
 }
